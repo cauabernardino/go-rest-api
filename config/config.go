0b7d3882d4dc0084bb11c736dd1bc9f70e8a116f
Cauã Bernardino <cbernardino@live.com>
2021-05-21 19:19:27 -0300

    Create config files

---
+++ config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// DBConnectString is the statement to connect the application with the database
+	DBConnectString = ""
+
+	// DB_PORT represents the database port
+	DB_PORT = 0
+
+	// API_PORT
+	API_PORT = 0
+)
+
+// LoadEnvs will initialize the environment variables for the application
+func LoadEnvs(arg string) {
+
+	var env string
+	if arg == "test" {
+		env = ".env.dev"
+	} else {
+		env = ".env"
+	}
+
+	// Load .env file
+	var err error
+	if err = godotenv.Load(env); err != nil {
+		log.Fatal(err)
+	}
+
+	// Get API_PORT and DB_PORT and check ports availability
+	API_PORT, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		API_PORT = 8000
+	}
+
+	DB_PORT, err = strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		DB_PORT = 5432
+	}
+
+	// Get DB related variables
+	DB_USER := os.Getenv("POSTGRES_USER")
+	DB_PASSWORD := os.Getenv("POSTGRES_PASSWORD")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_NAME := os.Getenv("POSTGRES_DB")
+
+	DBConnectString = fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		DB_USER,
+		DB_PASSWORD,
+		DB_HOST,
+		DB_PORT,
+		DB_NAME,
+	)
+
+}
