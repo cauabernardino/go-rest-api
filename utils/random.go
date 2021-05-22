35c4b7dec96a021e3c2da31b000af0610e906314
Cauã Bernardino <cbernardino@live.com>
2021-05-22 18:01:59 -0300

    Refactor of RandomPrice for asserting round numbers

--- utils/random.go
+++ utils/random.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"math"
 	"math/rand"
 	"strings"
 	"time"
@@ -12,15 +13,15 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-// RandomFloat generates a random float between a min and max given as integers
-func RandomFloat(min, max int64) float64 {
+// randomFloat generates a random float between a min and max given as integers
+func randomFloat(min, max int64) float64 {
 	intPart := float64(rand.Int63n(max - min + 1))
 
 	return float64(min) + intPart + rand.Float64()
 }
 
-// RandomString generates a random string of length n
-func RandomString(n int) string {
+// randomString generates a random string of length n
+func randomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
 
@@ -34,15 +35,17 @@ func RandomString(n int) string {
 
 // RandomName generates a random owner name
 func RandomName() string {
-	return RandomString(10)
+	return randomString(10)
 }
 
 // RandomPrice generates a random amount of money
 func RandomPrice() float64 {
-	return RandomFloat(0, 1000)
+	price := randomFloat(0, 1000)
+
+	return math.Round(price*100) / 100
 }
 
 // RandomDescription generates a random description
 func RandomDescription() string {
-	return RandomString(80)
+	return randomString(80)
 }
