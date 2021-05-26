1ee42d976bc161c58259258c516831caf0ad7d79
Cauã Bernardino <cbernardino@live.com>
2021-05-25 21:07:31 -0300

    Insertion of graceful shutdown for server.

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/cauabernardino/go-rest-api/config"
 	"github.com/cauabernardino/go-rest-api/db"
@@ -22,6 +27,32 @@ func main() {
 
 	router := router.GenerateLoggingRouter(router.GenerateRouter(db))
 
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", config.API_PORT),
+		Handler: router,
+	}
+
 	log.Printf("Listening of port %d...", config.API_PORT)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.API_PORT), router))
+
+	go func() {
+		server.ListenAndServe()
+	}()
+	defer stop(server)
+
+	serverSignal := make(chan os.Signal, 1)
+	signal.Notify(serverSignal, syscall.SIGINT, syscall.SIGTERM)
+
+	log.Println(fmt.Sprint(<-serverSignal))
+	log.Println("Stopping API server.")
+
+}
+
+// Stop handles graceful shutdown of server
+func stop(server *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Could not shut down server correctly: %v\n", err)
+		os.Exit(1)
+	}
 }
